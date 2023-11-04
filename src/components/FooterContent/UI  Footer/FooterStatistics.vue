68269a008549adfa34fb83f0812b45a9a421bfbe
Jeka <jonrovniy@yandex.ru>
2023-11-04 10:16:00 +0300

    переделал генерацию actions

--- src/components/FooterContent/UI  Footer/FooterStatistics.vue
+++ src/components/FooterContent/UI  Footer/FooterStatistics.vue
@@ -2,21 +2,52 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "FooterStatistics"
+  name: "FooterStatistics",
+  computed: {
+    infoForFooter() {
+      return [
+        {
+          titleField: 'characters',
+          valueField: 826, //будет в будущем проп
+          classField: 'mr-8 text--link',
+          action: this.clickOnCharacters
+        },
+        {
+          titleField: 'locations',
+          valueField: 122, //будет в будущем проп
+          classField: 'mr-8 text--link',
+          action: () => console.log('click on location')
+        },
+        {
+          titleField: 'episodes',
+          valueField: 51, //будет в будущем проп
+          classField: 'mr-8 text--link',
+          action: () => console.log('click on episodes')
+        },
+      ]
+    }
+  },
+  methods: {
+    clickOnCharacters() {
+      console.log('click on characters')
+    }
+  }
 })
 </script>
 
 <template>
   <div class="text-caption text-uppercase statistics font-weight-bold text-grey">
-    <span class="mr-8 text--link">characters: 826</span>
-    <span class="mr-8 text--link">locations: 122</span>
-    <span class="text--link">episodes: 51</span>
+    <span
+        v-for="(item, index) in infoForFooter"
+        :key="index"
+        @click="item.action"
+        :class="item.classField">{{ item.titleField }}: {{ item.valueField }}
+    </span>
   </div>
-
 </template>
 
 <style scoped lang="scss">
-  .statistics {
-    font-size: 14px !important;
-  }
+.statistics {
+  font-size: 14px !important;
+}
 </style>
